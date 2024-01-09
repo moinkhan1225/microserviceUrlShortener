4956145a91d6a02e601f743fc8afe9db80226912
Hakai <moinkhanrobin1@gmail.com>
2024-01-09 19:48:25 +0530

    connecting Mongo DB

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const mongoose = require('mongoose');
+
+//connecting mongoDB
+
+mongoose.connect(process.env.MONGO_URI)
+.then(console.log("Mongo is Connected"))
+.catch((e)=>{
+  console.error(e)
+})
+
+//creating schema
+
+const urlShrinker = mongoose.Schema({
+  originalUrl:{
+    type:String,
+    required
+  },
+  shortUrl:{
+    type:String,
+    required
+  }
+})
+
+//creating model
+const urlShrinkModel=mongoose.model('urlShrinkModel',urlShrinker)
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -14,11 +40,15 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
+
+//for Post
+app.post('/api/a',(req,res)=>{
+  const data = new data({
+    originalUrl    :   "asdasdas",
+    shortUrl       :   "asdasd"
+    })
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
